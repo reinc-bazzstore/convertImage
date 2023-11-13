e6965073ffde8e1a360410a5a6f1cd361362c9e6
reinc-bazzstore <150098801+reinc-bazzstore@users.noreply.github.com>
2023-11-13 17:52:16 +0900

    change to output image file name

--- convert_image.py
+++ convert_image.py
@@ -2,11 +2,11 @@ from rembg import remove
 from PIL import Image
 
 input_path = 'input.jpg'
-output_path = 'output.png'
+output_path = 'result-blankImage.png'
 
 input = Image.open(input_path)
 output = remove(input)
 
 background = Image.new('RGB', output.size, (255, 255, 255))
 background.paste(output, mask=output.split()[3])
-background.save('output.jpg')
+background.save('result-blankImage.jpg')
--- remove_background.py
+++ remove_background.py
@@ -2,10 +2,10 @@ from rembg import remove
 from PIL import Image
 
 input_path = 'input.jpg'
-output_path = 'output.png'
+output_path = 'result-removeBackground.png'
 
 input = Image.open(input_path)
 output = remove(input)
 output.save(output_path)
 
-img = Image.open('output.png')
+img = Image.open('result-removeBackground.png')
